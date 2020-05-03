10d076eab38d50be49a70d5484379d7431887dbc
Chenccy998 <1322093094@qq.com>
2020-05-03 20:21:43 +0800

    环境搭建完成，登录模块的service没有写

--- gmall-admin-web/src/main/java/com/atguigu/gmall/admin/GmallAdminWebApplication.java
+++ gmall-admin-web/src/main/java/com/atguigu/gmall/admin/GmallAdminWebApplication.java
@@ -2,8 +2,18 @@ package com.atguigu.gmall.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+/**
+ * @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+ * 1、不进行数据源的自动配置
+ * 如果导入的依赖引入一个自动配置的场景：
+ *     1）这个场景自动配置默认生效，我们就必须配置这个注解
+ *     2）不想配置
+ *        引入的时候排除这个场景的依赖
+ *        排除掉这个场景的自动配置类
+ */
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class GmallAdminWebApplication {
 
     public static void main(String[] args) {
